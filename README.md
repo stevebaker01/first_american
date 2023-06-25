177ba2ff9b9be20a0e7080ae07636c87d365d721
stevebaker01 <stevebaker01@gmail.com>
2023-06-24 18:53:59 -0700

    tests

--- README.md
+++ README.md
@@ -3,21 +3,37 @@
 
 I have created a service using FastAPI based upon the user story and a suite of 19 tests to run against it.
 
-git clone 
+* clone repo
+  * ssh
+    * `git clone git@github.com:stevebaker01/first_american.git`
+  * https
+    * `git clone https://github.com/stevebaker01/first_american.git`
+* `cd first_american`
 
+* set up environment
+  * `python3 -m venv venv`
+  * `source venv/bin/activate`
+  * `pip install -r requirements.txt`
+  
+* run is_name_matched service
+  * `uvicorn main:app --reload`
+  * view swagger docs:
+    * http://127.0.0.1:8000/docs
 
-The given user story is somewhat ambiguous which I assume is part of the exercise.
-(Please see included "TakeHome_TechnicalExercise.docx")
+* run the tests
+  * In a separate shell terminal navigate to the first_american directory.
+  * `source venv/bin/activate`
+  * `pytest test`
 
-> A name matching service is to be built that matches the
-Buyer names with Owner names.
->This service extracts up to first 4 characters of the last
-name from File A (Buyer) to File B (Owners). 
-> If there is a match, the flag is IsNameMatched is set to
-true.
+### Questions and Assumptions:
+1. The second sentence of the user story is somewhat strange. I have made the assumption that the author meant the following...
+> This service *compares* up to the first 4 characters of the last name*(s)* from File A (Buyer) to *up to the first 4 characters of the last name(s) from* File B (Owners).
+2. I have assumed that the POST body for the is_name_matched endpoint includes a full Buyer record and a full Owner (Assess) record. (see provided json)
+3. I have assumed that there can be multiple buyers per buyer record because the Buyer field in the buyer record is a list.
+4. I have assumed that a single match regardless number of buyers and owners will return *IsNameMatched=True*
+5. I have assumed that buyer and owner names can be represented in any case (title, upper or lower).
+6. I have assumed that no buyers and/or no owners will raise a 404 Bad Request error.
 
-The second sentence is a little strange. I am making an assumption that is should say
+All of these assumptions would precipitate questions to the production developer and/or product manager.
 
->This service compares up to first 4 characters of the last
-name from File A (Buyer) to up to the first four characters from File B (Owners). 
 
--- test/test_is_name_matched.py
+++ test/test_is_name_matched.py
@@ -7,7 +7,7 @@ MATCHED_URL = f"{BASE_URL}/is_name_matched"
 
 class TestIsNameMatchedFull:
 
-    def test_is_name_matched_match(self, session):
+    def test_is_name_matched_single_buyer_single_owner_match(self, session):
         """
         Single buyer and single owner match.
         """
@@ -17,16 +17,16 @@ class TestIsNameMatchedFull:
         assert res.status_code == 200
         assert res.json()["IsNameMatched"] is True
 
-    def test_is_name_matched_no_match(self, session):
+    def test_is_name_matched_single_buyer_single_owner_no_match(self, session):
         """
-        Singler buyer and single seller no match.
+        Single buyer and single owner no match.
         """
         json_data = match_name(buyers=prep_list(["baker steven a"]), owners=prep_list(["lee eric m"]))
         res = session.post(MATCHED_URL, data=json_data)
         assert res.status_code == 200
         assert res.json()["IsNameMatched"] is False
 
-    def test_is_name_matched_multiple_buyers_one_match(self, session):
+    def test_is_name_matched_multiple_buyers_one_owner_one_match(self, session):
         """
         Multiple buyers and a single owner match.
         """
@@ -37,7 +37,7 @@ class TestIsNameMatchedFull:
         assert res.status_code == 200
         assert res.json()["IsNameMatched"] is True
 
-    def test_is_name_matched_multiple_buyers_no_match(self, session):
+    def test_is_name_matched_multiple_buyers_single_owner_no_match(self, session):
         """
         Multiple buyers and a single owner on match.
         """
@@ -48,9 +48,9 @@ class TestIsNameMatchedFull:
         assert res.status_code == 200
         assert res.json()["IsNameMatched"] is False
 
-    def test_is_name_matched_multiple_owners_one_match(self, session):
+    def test_is_name_matched_single_buyer_multiple_owners_one_match(self, session):
         """
-        Multiple owners and a single seller match.
+        Multiple owners and a single owner match.
         """
         owners = prep_list([fake_name(), fake_name(), fake_name(), fake_name(), fake_name()])
         buyers = prep_list([choice(owners).upper()])
@@ -59,9 +59,9 @@ class TestIsNameMatchedFull:
         assert res.status_code == 200
         assert res.json()["IsNameMatched"] is True
 
-    def test_is_name_matched_multiple_owners_no_match(self, session):
+    def test_is_name_matched_single_buyer_multiple_owners_no_match(self, session):
         """
-        Multiple owners and a single seller no match.
+        Multiple owners and a single buyer no match.
         """
         buyers = prep_list(["BAKER STEVEN A", "LEE ERIC M", "DAVIDSON DAVID D", "STEVENS STEVEN"])
         owners = prep_list(["Jenner Bruce K"])
@@ -70,7 +70,7 @@ class TestIsNameMatchedFull:
         assert res.status_code == 200
         assert res.json()["IsNameMatched"] is False
 
-    def test_is_name_matched_multiple_owners_multiple_matches(self, session):
+    def test_is_name_matched_single_buyer_multiple_owners_multiple_matches(self, session):
         """
         Multiple owners and a single owner with multiple matches.
         """
@@ -105,9 +105,7 @@ class TestIsNameMatchedFull:
 
     def test_is_name_matched_multiple_buyers_multiple_owners_multiple_matches(self, session):
         """
-        Multiple buyers and multiple owners with mutiple matches.
-        :param session:
-        :return:
+        Multiple buyers and multiple owners with multiple matches.
         """
         buyers = prep_list(["Baker Steven A", "Baker Anne", "Baker Edwin C", "Baker Joanne I", "Cameron James"])
         owners = prep_list(["cameron james a", "SPEILBERG STEVEN B", "baker susan"])
@@ -117,58 +115,82 @@ class TestIsNameMatchedFull:
         assert res.json()["IsNameMatched"] is True
 
     def test_is_name_matched_one_word_name_match(self, session):
-
+        """
+        Single word names with match.
+        """
         json_data = match_name(buyers=prep_list(["MCFOOBAR"]), owners=prep_list(["mcfoobar"]))
         res = session.post(MATCHED_URL, data=json_data)
         assert res.status_code == 200
         assert res.json()["IsNameMatched"] is True
 
     def test_is_name_matched_one_word_name_no_match(self, session):
+        """
+        Single word names with no match.
+        """
         json_data = match_name(buyers=prep_list(["MCFOOBAR"]), owners=prep_list(["mcfubar"]))
         res = session.post(MATCHED_URL, data=json_data)
         assert res.status_code == 200
         assert res.json()["IsNameMatched"] is False
 
     def test_is_name_matched_matching_first_name_no_match(self, session):
+        """
+        Matching first names only, no match.
+        """
         json_data = match_name(buyers=prep_list(["baker steven"]), owners=prep_list(["stevens steven"]))
         res = session.post(MATCHED_URL, data=json_data)
         assert res.status_code == 200
         assert res.json()["IsNameMatched"] is False
 
     def test_is_name_matched_short_last_name_match(self, session):
+        """
+        Short lasts names (2 chars) with a match.
+        """
         json_data = match_name(buyers=prep_list(["HO KATE"]), owners=prep_list(["HO BART"]))
         res = session.post(MATCHED_URL, data=json_data)
         assert res.status_code == 200
         assert res.json()["IsNameMatched"] is True
 
     def test_is_name_matched_short_last_name_no_match(self, session):
+        """
+        Short last names (2 chars) no match.
+        """
         json_data = match_name(buyers=prep_list(["HO KATE"]), owners=prep_list(["HA KATE"]))
         res = session.post(MATCHED_URL, data=json_data)
         assert res.status_code == 200
         assert res.json()["IsNameMatched"] is False
 
     def test_is_name_matched_ambiguous_first_four_chars_no_match(self, session):
+        """
+        Ambiguous names. Buyer first and last name 4 chars altogether with a owner last name of the same 4 chars.
+        No match.
+        """
         json_data = match_name(buyers=prep_list(["HO LA"]), owners=prep_list(["HOLA KATE"]))
         res = session.post(MATCHED_URL, data=json_data)
         assert res.status_code == 200
         assert res.json()["IsNameMatched"] is False
 
     def test_is_name_matched_no_buyers_raises_404(self, session):
-
+        """
+        No buyers raises a 404 error with appropriate message.
+        """
         json_data = match_name(buyers=[], owners=prep_list([fake_name()]))
         res = session.post(MATCHED_URL, data=json_data)
         assert res.status_code == 404
         assert res.json() == dict(detail="Buyer record must contain one or more Buyer.")
 
     def test_is_name_matched_no_owners_raises_404(self, session):
-
+        """
+        No owners raises a 404 error with appropriate message.
+        """
         json_data = match_name(buyers=prep_list([fake_name()]), owners=[])
         res = session.post(MATCHED_URL, data=json_data)
         assert res.status_code == 404
         assert res.json() == dict(detail="Assess record must contain one or more Owners.")
 
     def test_is_name_matched_no_owners_and_no_buyers_raises_404(self, session):
-
+        """
+        No buyers or owners raises a 404 error with appropriate message.
+        """
         json_data = match_name(buyers=[], owners=[])
         res = session.post(MATCHED_URL, data=json_data)
         assert res.status_code == 404
